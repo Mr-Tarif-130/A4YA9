6eb2cad9c085ab625a99b119d1919d30dc119a9d
Mr-Tarif-130 <mr.tarif130@gmail.com>
2024-10-09 18:52:16 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ Ꮇʀᵀᴬᴿᴵᶠ~Ұ₮࿐x𝟔130 ]";
+const doNotDelete = "[   Ꮇʀᵀᴬᴿᴵᶠ~Ұ₮࿐𝐱130  ]";
 /**
 * @author NTKhang
 * @author: do not delete it
